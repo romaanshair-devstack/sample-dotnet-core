2f2d01ecea5dcd1d41e5530a2fc9ab18f57546d4
Romaan Shair <roshair@microsoft.com>
2020-03-24 14:20:02 +0530

    add unit tests

---
+++ TestProjectOne/UnitTest1.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjectOne
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 5;
+            int b = 5;
+            Assert.AreEqual(a, b);
+        }
+
+        public void TestMethod2()
+        {
+            int a = 5;
+            int b = 5;
+            Assert.AreEqual(a, b);
+
+            public void TestMethod3()
+            {
+                int a = 5;
+                int b = 5;
+                Assert.AreEqual(a, b);
+            }
+
+            public void TestMethod4()
+            {
+                int a = 5;
+                int b = 5;
+                Assert.AreEqual(a, b);
+            }
+        }
+    }
+}
