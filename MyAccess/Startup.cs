cbcefe3bf8ea8690a685d0404dcc54799119bcde
romaanshair-devstack <62882230+romaanshair-devstack@users.noreply.github.com>
2020-03-31 10:35:01 +0530

    Update Startup.cs

--- MyAccess/Startup.cs
+++ MyAccess/Startup.cs
@@ -36,7 +36,7 @@ namespace MyAccess
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
